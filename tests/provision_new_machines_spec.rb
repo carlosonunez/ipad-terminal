10080767fa27c7e1902cefc3de60eaf56ff43586
Carlos Nunez <carlosonunez@noreply.github.com>
2020-07-03 10:26:20 -0500

    Add command to create machines
    This is a tough decision. I want to write my tests in Rspec despite my
    codebase being in Shell because it is much nicer than writing tests with
    Bats. Do I install Ruby on my system and use `system` to call Docker
    commands (because I don't want to install Terraform et. al. on my
    machine)? Or do I create a "fat" Docker image with everything I need in
    it and run everything from Docker?
    
    I opted for the fat image route since that's easier to get going with
    and avoids all of the "fun" that comes with abstracting Docker commands.

--- tests/provision_new_machines_spec.rb
+++ tests/provision_new_machines_spec.rb
@@ -1,7 +1,14 @@
 require 'rspec'
 
+def create_new_machine
+  system("terraform apply")
+end
+
 describe "When creating new machines" do
   before(:all) do
+    if !create_new_machine!
+      raise "Unable to create new machine. Check logs for more info."
+    end
   end
 
   example "Then we get an IP address" do
