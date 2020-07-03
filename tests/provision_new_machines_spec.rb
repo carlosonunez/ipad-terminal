f31fd04a34f7419de5fe9fd20f63b34ad0b36f0d
Carlos Nunez <carlosonunez@noreply.github.com>
2020-07-03 10:21:14 -0500

    Add tests shell.

---
+++ tests/provision_new_machines_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+describe "When creating new machines" do
+  before(:all) do
+  end
+
+  example "Then we get an IP address" do
+  end
+
+  example "Then we get a SSH private key", :wip do
+  end
+end
